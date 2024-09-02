832993eac07a36007761fa50d290a694faae526e
pendulum <rs.pendulum@gmail.com>
2024-09-02 18:25:19 +0300

    test tabs

--- src/bst/bst.structure.ts
+++ src/bst/bst.structure.ts
@@ -2,7 +2,7 @@ export class TreeNode {
 	public val: number;
 	public left: TreeNode | null;
 	public right: TreeNode | null;
-
+  
 	constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
 		this.val = val === undefined ? 0 : val;
 		this.left = left === undefined ? null : left;
