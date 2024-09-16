a569aa3ec2328f8845977c5aa4220f21dd7ffc19
pendulum <rs.pendulum@gmail.com>
2024-09-16 12:58:49 +0300

    added dbg for maxProfit()

--- src/array/max-profit.ts
+++ src/array/max-profit.ts
@@ -12,3 +12,28 @@ export function maxProfit(prices: number[]): number {
 
   return maxProfit;
 }
+
+export function maxProfitDBG(){
+  
+  const tests = [
+    {
+      input: [7, 1, 5, 3, 6, 4],
+      result: 5 // Купить за 1 и продать за 6
+    },
+    {
+      input: [7, 6, 4, 3, 1],
+      result: 0 // Прибыль не возможна
+    }
+  ];
+  
+  tests.forEach((test, index) => {
+    const result = maxProfit(test.input);
+    if (result === test.result) {
+      console.log(`${index} success`);
+    } else {
+      console.log(`${index} fail`);
+      console.log(`expected ${test.result}`);
+      console.log(`got ${result}`);
+    }
+  });
+}
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { majorityElementDBG } from "./array/majority-element";
+import { maxProfitDBG } from "./array/max-profit";
+import { productExceptSelfDBG } from "./array/product-except-self";
 import { printTree, TreeNode } from "./bst";
 import { lowestCommonAncestorDBG } from "./bst/problems/lowest-common-ancestor";
 import { HitCounterDBG } from "./scratch_structure/hit-counter";
 import { longestPalindromeDBG } from "./string/longest-palindrome";
 
-longestPalindromeDBG()
+maxProfitDBG()
