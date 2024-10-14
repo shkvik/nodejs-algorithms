3eb22fd1a04e9451af3a50a54ee8b6475d895a6c
pendulum <rs.pendulum@gmail.com>
2024-10-14 16:35:29 +0300

    added removeCoveredIntervals()

--- src/array/intervals/remove-covered-intervals.ts
+++ src/array/intervals/remove-covered-intervals.ts
@@ -1,10 +1,7 @@
-import { checkInclusionDBG } from "./string/two_pointers/check-inclusion";
-
-const input = [[0,10],[0,11],[5,12]];
-
-
-[].toSorted
-
+/**
+ * @problem 1288. Remove Covered Intervals
+ * @link https://leetcode.com/problems/remove-covered-intervals
+ */
 function removeCoveredIntervals(intervals: number[][]): number {
   intervals.sort((a, b) =>
     a[0] === b[0] ? b[1] - a[1] : a[0] - b[0]
@@ -12,14 +9,11 @@ function removeCoveredIntervals(intervals: number[][]): number {
   let count = 0;
   let maxEnd = 0;
 
-  for (const [_, end] of intervals) {
+  for (const [start, end] of intervals) {
     if (end > maxEnd) {
       count++;
       maxEnd = end;
     }
   }
-
   return count;
 };
-
-console.log(removeCoveredIntervals(input));
