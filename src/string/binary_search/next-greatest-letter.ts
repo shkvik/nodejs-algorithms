946fa8c9efec994cf055df8973702724e1955ad3
pendulum <rs.pendulum@gmail.com>
2024-10-21 17:30:02 +0300

    added nextGreatestLetter()

--- src/string/binary_search/next-greatest-letter.ts
+++ src/string/binary_search/next-greatest-letter.ts
@@ -1,8 +1,22 @@
-
-
+/**
+ * @problem [744. Find Smallest Letter Greater Than Target](https://leetcode.com/problems/find-smallest-letter-greater-than-target)
+ * 
+ * ### Решаем бинарным поиском
+ * 1. по условию задачи надо понять, что можно сразу отдать первый символ,
+ * если target больше или равен последнему
+ * 2. ну а если так нельзя, то bs'ом двигаем left и right до талого и возвращаем
+ * и возвращаем left (самый маленький)
+ * 
+ * Ключевой момент в том, чтобы двигать левый указатель, когда символы равны
+ * ```
+ * if (letters[mid] <= target) {
+ *   left = mid + 1;
+ * }
+ * ```
+ */
 function nextGreatestLetter(letters: string[], target: string): string {
   let left = 0, right = letters.length - 1;
-  
+
   if (target >= letters[right]) {
     return letters[0];
   }
